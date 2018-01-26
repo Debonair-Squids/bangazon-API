41dd80761360f788f994fe912714821f154b288f
Jenna Solis <jennabsol@gmail.com>
2018-01-26 13:08:10 -0600

    payment type contoller done

--- Controllers/PaymentTypeController.cs
+++ Controllers/PaymentTypeController.cs
@@ -13,10 +13,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace bangazon_inc.Controllers
 {
+    //sets the route to the name of the website/'Department'
     [Route("[controller]")]
 
 
-     // Class to PUT/POST/GET/DELETE payment type
+    // Class to PUT/POST/GET/DELETE payment type
     public class PaymentTypeController : Controller
     {
         //Sets up an empty variable _context that will  be a reference of our BangazonAPIContext class
@@ -26,9 +27,8 @@ namespace bangazon_inc.Controllers
             _context = ctx;
         }
 
-        // Get All payment type
-        // ex. url/paymenttype
-        // returns all payment types if any exist
+        // Get All PaymentType PaymentType http://localhost:5000/PaymentType/9
+
 
         [HttpGet]
         public IActionResult Get()
@@ -44,44 +44,49 @@ namespace bangazon_inc.Controllers
 
         }
 
-        // // Get a single payment type by PaymentTypeId
-        // // ex. paymenttype/4 etc.
-        // [HttpGet("{id}", Name = "GetSinglePaymentType")]
-        // public IActionResult Get([FromRoute] int id)
+        // Get Single PaymentType PaymentType http://localhost:5000/PaymentType/1
+        [HttpGet("{id}", Name = "GetSinglePaymentType")]
+        public IActionResult Get([FromRoute] int id)
+        {
+            //if you request anything other than an Id you will get a return of BadRequest.
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                //will search the _context.PaymentType for an entry that has the id we are looking for
+                //if found, will return that PaymentType
+                //if not found will return 404.
+                PaymentType singlePaymentType = _context.PaymentType.Single(m => m.PaymentTypeId == id);
+
+                if (singlePaymentType == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(singlePaymentType);
+            }
+            //if the try statement fails for some reason, will return error of what happened.
+            catch (System.InvalidOperationException ex)
+            {
+                return NotFound(ex);
+            }
+        }
+        // Helper method to check if the payment type exists in the database
+        private bool PaymentTypeExists(int PaymentTypeId)
+        {
+            return _context.PaymentType.Count(e => e.PaymentTypeId == PaymentTypeId) > 0;
+        }
+        // PaymentType http://localhost:5000/PaymentType/
+        // Object:
         // {
-        //     if (!ModelState.IsValid)
-        //     {
-        //         return BadRequest(ModelState);
-        //     }
-
-        //     try
-        //     {
-        //         PaymentType paymentType = _context.PaymentType.Single(m => m.PaymentTypeId == id);
-
-        //         if (paymentType == null)
-        //         {
-        //             return NotFound();
-        //         }
-
-        //         return Ok(paymentType);
-        //     }
-        //     catch (System.InvalidOperationException ex)
-        //     {
-        //         return NotFound(ex);
-        //     }
-        // }
-
-        // Post a new payment type
-        // ex. /paymenttype
-        // Requires an Object:
-        // {
-        //     "Name": "Example",
-        //     "AccountNumber": ex. 1123,
-        //     "CustomerID": ex. 1
+        //     "PaymentTypeName": "Visa"
         // }
 
         [HttpPost]
-        public IActionResult Post([FromBody] PaymentType newPaymentType)
+        public IActionResult Post([FromBody]PaymentType newPaymentType)
         {
             if (!ModelState.IsValid)
             {
@@ -108,75 +113,69 @@ namespace bangazon_inc.Controllers
 
             return CreatedAtRoute("GetSinglePaymentType", new { id = newPaymentType.PaymentTypeId }, newPaymentType);
         }
-        // Helper method to check if the payment type exists in the database
-        private bool PaymentTypeExists(int PaymentTypeId)
+
+
+
+        // Edit PaymentType http://localhost:5000/PaymentType/9
+        // Object:
+        // {
+        //     "PaymentTypeId": 9,
+        //     "PaymentTypeName": "Master"
+        // }
+        // Can not change PaymentTypeId
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]PaymentType editPaymentType)
         {
-          return _context.PaymentType.Count(e => e.PaymentTypeId == PaymentTypeId) > 0;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != editPaymentType.PaymentTypeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(editPaymentType).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new StatusCodeResult(StatusCodes.Status204NoContent);
         }
 
-        // Edit a payment type
-        // ex. /paymenttype/4
-        // Requires an Object:
-        // {
-        //     "PaymentTypeId": ex. 1,
-        //     "AccountNumber": ex. 2,
-        //     "CustomerID": ex. 3,
-        //     "Name": "Example"
-        // }
-        // [HttpPut("{id}")]
-        // public IActionResult Put(int id, [FromBody] PaymentType modifiedPaymentType)
-        // {
-        //     if (!ModelState.IsValid)
-        //     {
-        //         return BadRequest(ModelState);
-        //     }
+        // Deletes PaymentType http://localhost:5000/PaymentType/9
 
-        //     if (id != modifiedPaymentType.PaymentTypeId)
-        //     {
-        //         return BadRequest();
-        //     }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
-        //     _context.Entry(modifiedPaymentType).State = EntityState.Modified;
+            PaymentType deletePaymentType = _context.PaymentType.Single(m => m.PaymentTypeId == id);
+            if (deletePaymentType == null)
+            {
+                return NotFound();
+            }
+            _context.PaymentType.Remove(deletePaymentType);
+            _context.SaveChanges();
 
-        //     try
-        //     {
-        //         _context.SaveChanges();
-        //     }
-        //     catch (DbUpdateConcurrencyException)
-        //     {
-        //         if (!PaymentTypeExists(id))
-        //         {
-        //             return NotFound();
-        //         }
-        //         else
-        //         {
-        //             throw;
-        //         }
-        //     }
-
-        //     return new StatusCodeResult(StatusCodes.Status204NoContent);
-        // }
-
-        // // Deletes a payment type
-        // // ex. paymenttype/4
-        // [HttpDelete("{id}")]
-        // public IActionResult Delete(int id)
-        // {
-        //     if (!ModelState.IsValid)
-        //     {
-        //         return BadRequest(ModelState);
-        //     }
-
-        //     PaymentType paymentType = _context.PaymentType.Single(m => m.PaymentTypeId == id);
-        //     if (paymentType == null)
-        //     {
-        //         return NotFound();
-        //     }
-
-        //     _context.PaymentType.Remove(paymentType);
-        //     _context.SaveChanges();
-
-        //     return Ok(paymentType);
-        // }
+            return Ok(deletePaymentType);
+        }
     }
 }
